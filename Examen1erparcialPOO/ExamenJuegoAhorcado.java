6c5d9e874404d04277d3a9743ea6ac8c3b69a97e
LuisDavid2003 <l.raya.m457@edu.utc.mx>
2023-10-11 07:26:06 -0600

    Examen 1er parcial

--- Examen1erparcialPOO/ExamenJuegoAhorcado.java
+++ Examen1erparcialPOO/ExamenJuegoAhorcado.java
@@ -1,14 +1,22 @@
+//Alumno: Raya Morales Luis David
+//Grupo: 4ZLEINSCMA
+//Examen 1er parcial (opcion 1: Juego del Ahorcado)
+//Docente: Jaime Minor Gomez
+//11-octubre-2023
+//Materia: Programacion orientada a objetos 
+
 import java.util.Scanner;
 public class ExamenJuegoAhorcado{
    public static void main(String args[])
    {
       Scanner sc = new Scanner(System.in);
-      //Creando variables
+      //Primero, Creamos las variables
       int intentos=5,W=0;
       boolean VD=true,w=true;
       int rep=0;
-      //Ingresando palabra pa buscar
-      System.out.println("Ingrese palabra");
+      //Se ingresa la palabra para adivinar
+    
+      System.out.println("Introduzca la palabra que desee que adivinen");
       String palabra=sc.nextLine();
       System.out.println("\n\n");
       String letra[]=new String[5];
@@ -20,12 +28,12 @@ public class ExamenJuegoAhorcado{
       W=letras.length-1;
       for(int i=0;i<25;i++){System.out.println("");}
       for(int i=1;i<letras.length;i++){
-         intento[i]="_ ";//Asignando lineas de la palabra ingresada asi la variable intento
+         intento[i]="_ ";//Se asignan lineas de la palabra ingresada asi la variable (intento)
                                             }
        //Teniendo 5 intentos mientras no se acaben se repetiran las, siguientes acciones
       while(intentos>0)
       {   
-        System.out.println("Ingrese letra");
+        System.out.println("Introduzca alguna letra");
         for(int i=1;i<letras.length;i++)
                  {    //Mostrando lineas de la palabra ingresada
                      System.out.print(intento[i]);
@@ -36,11 +44,11 @@ public class ExamenJuegoAhorcado{
            {
              w=true;
             //si la letra ingresada se encuentra en parte de las letras que forman parte de la palabra
-            //ingresada entrara
+            //ingresada entrara lo siguiente:
             if(letras[i].equals(letra[0]))
-            {  //si la pabra no fue ingresada antes entrara
+            {  //si la palabra no fue ingresada antes entrara lo siguiente:
                if(!intento[i].equals(letra[0])){
-                  //Asignando a la variable intento la letra ingresada
+                  //Asignando a la variable (intento) la letra ingresada
                   intento[i]=letra[0];
                   VD=false;
                   rep++; ingresadas[i]=letras[0];  
@@ -48,7 +56,7 @@ public class ExamenJuegoAhorcado{
             }
                                                      }  
              //en caso ya se aiga introducido x letra que ya estaba aparecera este mensaje
-             if(aviso==false){System.out.println(" Aviso: Ya ingresaste esa letra ");}
+             if(aviso==false){System.out.println(" Enhorabuena: Ya ingresaste esa letra ");}
              aviso=true;        
              if(VD==true)
              { //reduciendo intentos
@@ -57,14 +65,13 @@ public class ExamenJuegoAhorcado{
              else{W=W-rep;} System.out.println(" \nQuedan: "+intentos+" Intentos");
              //llamando la funcion impresion que dibuja el ahorcado
              impresion(intentos);
-             if(intentos==0){System.out.println("\nLastima Perdiste\nLa palabra es: "+palabra);}
-             if(W<=0){System.out.println(palabra+"\nFelicidades ganaste, Bien echo.");intentos=0;}
+             if(intentos==0){System.out.println("\nHas perdido, suerte para la proxima. Gracias por jugar\nLa palabra es: "+palabra);}
+             if(W<=0){System.out.println(palabra+"\nFelicidades has ganado el juego, Bien echo. Gracias por jugar");intentos=0;}
              VD=true;rep=0;
                       
-             
-          }
+            }    
    }
-     //void impresion void que usare para imprimir el dibujo del ahorcado
+     //void impresion void que se usara para imprimir el dibujo ilustrativo al juego del ahorcado
    static void impresion(int vd)
    {
     if(vd==5)
