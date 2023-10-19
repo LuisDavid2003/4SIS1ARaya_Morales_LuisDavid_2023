96411922a06c7ca276f7032df845fa653c8d2613
LuisDavid2003 <l.raya.m457@edu.utc.mx>
2023-10-18 18:57:58 -0600

    mas cadenas

--- 07Cadenas/Principal.java
+++ 07Cadenas/Principal.java
@@ -2,9 +2,11 @@ public class Principal {
     
     public static void main(String[] args){
         //cadena objcadena = new cadena();
-        CadenaInvertida invertida = new CadenaInvertida();
+        //CadenaInvertida invertida = new CadenaInvertida();
         //objcadena.cadenas();
-        invertida.cadenaInvertidaPalabras();
+        //invertida.cadenaInvertidaPalabras();
+        masCadenas operaciones = new masCadenas();
+        operaciones.masFuncionesCadena();
     }
 }
 
