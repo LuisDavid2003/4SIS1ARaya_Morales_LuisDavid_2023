34052747d4404bcb1a37ef766e1d59e8e64884e3
LuisDavid2003 <l.raya.m457@edu.utc.mx>
2023-11-14 18:58:45 -0600

    Titanes

---
+++ Attack on titan/Titan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.titan;
+
+/**
+ *
+ * @author HPSPECTRE-ROBERTO
+ */
+public class Titan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
